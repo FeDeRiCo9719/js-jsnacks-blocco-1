ad94bed14d09fb81c5c4a5583c8616a19a316db7
FeDeRiCo9719 <federico.lovaglio.1997@gmail.com>
2021-08-26 12:38:32 +0200

    while per il controllo dato

--- Js/script.js
+++ Js/script.js
@@ -2,22 +2,42 @@
 
 // 1. Array vuoto (contenitore per i valori filtrati) e un array di supporto (contenitore per i tutti i valori)
 // 2. Chiedere 6 volte l'inserimento di un numero casuale (prompt *6 = ciclo definito *6)
-// 3. Filtrare gli input dati dall'utente e assegnarli all' array (se tra gli input ci sono numeri dispari = if input % 2 = 1 allora..  )
+// 3. Verificare che il dato inserito sia un numero
+// 4. Filtrare gli input dati dall'utente e assegnarli all' array (se tra gli input ci sono numeri dispari = if input % 2 = 1 allora..  )
+// 5. Stampare l' Output dei possibili casi
 
 
-// 1. Array vuoto e array di supporto
+// 1. Array vuoto, array di supporto e variabile di controllo
 var array = [];
 var supporto = [];
+var booleana = 0;
 
 // 2. Chiedere 6 volte l'inserimento di un numero casuale (prompt *6 = ciclo definito *6)
-for ( var i = 0; i < 6; i++ ) {
-    supporto.push(parseInt(prompt("inserisci un numero casuale")));
-    // console.log(supporto[i]);
+for ( var i = 0; i < 2; i++ ) {
 
-    // 3. Filtrare gli input dati dall'utente e assegnarli all' array
-    if (supporto[i] % 2 == 1) {
-        array.push(supporto[i]);
+    supporto[i] = (parseInt(prompt("inserisci un numero casuale")));
+
+    // 3. Verificare che il dato inserito sia un numero
+    while (isNaN(supporto[i])) {
+        alert("carattere non valido, riprova");
+        supporto[i] = (parseInt(prompt("inserisci un numero casuale")));
     }
+    alert("Il numero inserito è " + supporto[i]);
+
+    // 4. Filtrare gli input dati dall'utente e assegnarli all' array
+    if (supporto[i] % 2 == 1) {
+        array [i] = (supporto[i]);
+        booleana = 1;
+    }
+    console.log( "Il valore singolo di array è " + array[i]);
 }
-// Output - stampo array filtrato
-console.log("Il valore di array è " + array);
+console.log(booleana);
+
+// 5. Stampare l' Output dei possibili casi
+if (booleana==0) {
+    console.log("L'array è vuoto!");
+    alert("L'array è vuoto!");
+} else {
+    console.log("Il valore totale di array è " + array);
+    alert("Il valore totale di array è " + array);
+}
